61cf28ba939c5a380a570b46f2608ca620866da5
Lucero Raul <luceroraul12@gmail.com>
2022-11-19 19:29:08 -0300

    first commit

---
+++ src/main/java/testspringdatajpa/TestSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package testspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testspringdatajpa/TestSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package testspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
